fdbc85e1ea78a52ebd804ace0d7a3c9dbfeeaf9b
hexu <hexu_1985@sina.com>
2018-02-15 11:14:15 +0800

    add prog1.4 of cpp.algo.part1-4

---
+++ cpp.algo.part1-4/chap01/prog1.4-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "common.h"
+
+using namespace std;
+
+static const int N = 10;
+
+int main()
+{ 
+    int i, j, p, q, id[N], sz[N];
+    for (i = 0; i < N; i++) { 
+        id[i] = i; sz[i] = 1; 
+    }
+
+    cout << "p q    0 1 2 3 4 5 6 7 8 9 \n" << endl;
+    while (cin >> p >> q) { 
+        for (i = p; i != id[i]; i = id[i]) 
+            id[i] = id[id[i]]; 
+        for (j = q; j != id[j]; j = id[j]) 
+            id[j] = id[id[j]]; 
+        if (i == j) continue;
+        if (sz[i] < sz[j]) { 
+            id[i] = j; sz[j] += sz[i]; 
+        } else { 
+            id[j] = i; sz[i] += sz[j]; 
+        }
+
+        cout << " " << p << " " << q << endl;
+        print(id, N);
+    }
+}
+
